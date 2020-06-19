ba4686341d7371d04968b6b3382275cbbf0db594
nithin.bc <nithinchandran@hotmail.com>
2020-06-19 19:19:56 +0530

    added

--- CodeItHere/IMS.TimeSheet/IMS.cs
+++ CodeItHere/IMS.TimeSheet/IMS.cs
@@ -55,6 +55,7 @@ namespace IMS.TimeSheet
             {
                 details = GetLogDetailsFromRange(range);
             }
+            Log(details);
 
         }
         private IList<LogData> GetLogDetailsFromRange(string range, int hours = 8, int minutes = 0)
@@ -108,10 +109,6 @@ namespace IMS.TimeSheet
 
             return result;
         }
-        private string GetSprint()
-        {
-            return null;
-        }
         private IList<LogData> GetLogDetailsFromTimeSheet()
         {
             IList<LogData> result = null;
@@ -124,21 +121,87 @@ namespace IMS.TimeSheet
                     var tds = elements[i].FindElements(By.TagName("td"));
                     if (string.IsNullOrWhiteSpace(tds[0].Text) || string.IsNullOrWhiteSpace(tds[4].Text) || string.IsNullOrWhiteSpace(tds[5].Text))
                         continue;
+                    if (tds[5].Text != "0.0" || tds[4].Text == "00:00")
+                        continue;
                     var total = tds[4].Text.Split(':');
                     var logged = tds[5].Text.Split('.');
                     DateTime.TryParseExact(tds[0].Text, "M/d/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date);
-                    result.Add(new LogData
+                    var log = new LogData
                     {
                         Date = date,
                         TotalHours = Convert.ToInt32(total[0]),
                         TotalMinutes = total.Length > 1 ? Convert.ToInt32(total[1]) : 0,
-                        LoggedHours = Convert.ToInt32(logged[0]),
-                        LoggedMinutes = logged.Length > 1 ? Convert.ToInt32(logged[1]) : 0,
-                    });
+                        //LoggedHours = Convert.ToInt32(logged[0]),
+                        //LoggedMinutes = logged.Length > 1 ? Convert.ToInt32(logged[1]) : 0,
+                    };
+                    var span = new TimeSpan(log.TotalHours, log.TotalMinutes, 0).Subtract(new TimeSpan(0,30,0));
+                    if(span.TotalMinutes< 7 * 60)
+                    {
+                        log.LoggedHours = log.TotalHours;
+                        log.LoggedMinutes = log.TotalMinutes;
+                    }
+                    else
+                    {
+                        log.LoggedHours = span.Hours;
+                        log.LoggedMinutes = span.Minutes;
+                    }
+                    result.Add(log);
                 }
             }
 
             return result;
+        }
+        private bool Log(IList<LogData> details, string sprint = "SP2020-EGT1")
+        {
+            
+
+            for (int i = 0; i < details.Count; i++)
+            {
+                _driver.FindElement(By.Id("GenerlTask")).Click();
+                WaitForLoading();
+                var log = details[i];
+                _driver.FindElement(By.Id("Descrip")).SendKeys("Support to team members");
+                _javaScriptExecutor.ExecuteScript($"$('#Nontfsdate').val('{log.Date.ToString("dd'/'MM'/'yyyy")}')");
+                _javaScriptExecutor.ExecuteScript("$('#hours').val('" +
+                                                                    (log.LoggedHours > 9 ? log.LoggedHours.ToString() : "0" + log.LoggedHours.ToString()) +
+                                                                    ":" +
+                                                                    (log.LoggedMinutes > 9 ? log.LoggedMinutes.ToString() : "0" + log.LoggedMinutes.ToString()) +
+                                                                    "')");
+
+                var select = new SelectElement(_driver.FindElement(By.Id("project")));
+                select.SelectByValue("SPSA");
+                WaitForLoading();
+                select = new SelectElement(_driver.FindElement(By.Id("subproject")));
+                select.SelectByValue(GetSprint(sprint));
+                WaitForLoading();
+                select = new SelectElement(_driver.FindElement(By.Id("activity")));
+                select.SelectByValue("Technical");
+                WaitForLoading();
+                _driver.FindElement(By.Id("Button1")).Click();
+                WaitForLoading();
+            }
+
+            return true;
+        }
+        private string GetSprint(string prefix)
+        {
+            var select = new SelectElement(_driver.FindElement(By.Id("subproject")));
+            int large = 0;
+            for (int i = 0; i < select.Options.Count; i++)
+            {
+                var regex = new Regex("^" + prefix + @"-(\d+)$");
+                var text = select.Options[i].Text;
+                if (regex.IsMatch(text))
+                {
+
+                    var matches = regex.Match(text);
+                    var number = Convert.ToInt32(matches.Groups[1].Value);
+                    if (number > large)
+                        large = number;
+                }
+
+            }
+            return prefix + "-" + large.ToString();
         }
         public class LogData
         {
@@ -149,7 +212,7 @@ namespace IMS.TimeSheet
             public int TotalMinutes { get; set; }
             public override string ToString()
             {
-                return Date.ToString("dd-MM-yyyy"); 
+                return Date.ToString("dd-MM-yyyy");
             }
         }
     }
