77060774fb668ae6564c10e05c79f737f1bfe0e8
nithin.bc <nithin.bc@pitsolutions.com>
2022-02-28 11:27:01 +0530

    alogorithm project added

---
+++ CodeItHere/Algorithms/Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static class Array
+    {
+        public static void Shuffle<T>(this T[] array, int seed)
+        {
+            //Fisher-Yates algorithm AKA the Knuth Shuffle
+            var rnd = new Random(seed);
+            int n = array.Length;
+            while (n > 1)
+            {
+                int swappableIndex = rnd.Next(n--);
+                T temp = array[n];
+                array[n] = array[swappableIndex];
+                array[swappableIndex] = temp;
+            }
+        }
+        public static void Unshuffle<T>(this T[] array, int seed)
+        {
+            var rnd = new Random(seed);
+            int n = array.Length;
+            int[] rSequence = new int[n - 1];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                rSequence[i] = rnd.Next(n--);
+            }
+            for (int i = 1; i < array.Length; i++)
+            {
+                int swappableIndex = rSequence[^i];
+                T temp = array[i];
+                array[i] = array[swappableIndex];
+                array[swappableIndex] = temp;
+            }
+        }
+    }
+}
---
+++ CodeItHere/Algorithms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayShufflingExample();
+        }
+
+        static void ArrayShufflingExample()
+        {
+            Console.WriteLine("Original");
+            var random = new Random();
+            var array = new int[] { 1, 5, 6, 8, 23, 56, 98, 456, 255, 266, 666, 666, 2, 4, 9 };
+            //var array = Enumerable.Range(1, 10).Select(x=>random.Next(100)).ToArray();
+            Console.WriteLine(string.Join(',', array.Select(x => x.ToString())));
+
+            Console.WriteLine("Shuffled");
+            array.Shuffle<int>(25);
+            Console.WriteLine(string.Join(',', array.Select(x => x.ToString())));
+
+            Console.WriteLine("Reshuffled");
+            array.Unshuffle<int>(25);
+            Console.WriteLine(string.Join(',', array.Select(x => x.ToString())));
+
+        }
+    }
+}
